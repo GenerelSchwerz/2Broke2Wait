daa04e0cfc29fc399a500f7807f6273d803e0db3
GenerelSchwerz <xXSG.KingXx@gmail.com>
2022-12-28 20:11:35 -0500

    push

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,49 @@
 import config from "config"
 import { buildClient } from "./discord/index.js"
+import {Conn as RobConn} from "@rob9315/mcproxy"
+import merge from "ts-deepmerge";
 
-const token = config.get("discord.token")
+import {BotOptions} from "mineflayer"
+// const token = config.get("discord.token")
 
-const discClient = await buildClient(token, "!")
+
+const options = {
+    "discord": {
+        "token": process.env.BOT_TOKEN,
+        "prefix": "!"
+    },
+    "minecraft": {
+        "account": {
+            "username": "Generel_Schwerz",
+            "email": "fuck",// shit,
+            "password": "fuck",
+            "auth": "offline"
+        },
+        "server": {
+            "host": "localhost",
+            "port": 25565,
+            "version": "1.18.2",
+        },
+
+      
+    } 
+}
+
+function buildBotOpts(opts: typeof options): BotOptions {
+    return merge.default(opts.minecraft.account, opts.minecraft.server) as BotOptions
+}
+
+
+// const discClient = await buildClient(options.discord.token, options.discord.prefix)
+
+
+console.log(buildBotOpts(options))
+
+const proxyClient = new RobConn(buildBotOpts(options))
+
+const bot = proxyClient.stateData.bot._client
+
+bot.on("packet", (data, meta) => {
+    console.log(meta.name)
+})
 
